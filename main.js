69b0fbe73bcf5befb108951fcccaa7887025cba5
Hasib24 <hasibislam1488@gmail.com>
2022-11-07 03:01:04 +0600

    add toggle night mood

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 
-var disp = document.querySelectorAll('.display')[0];
+var disp = document.querySelectorAll('.resultDiv')[0];
+var modeIcon = document.querySelectorAll('.blankDiv')[0];
+const hasibBody = document.querySelectorAll('.hasib')[0];
+const hasibBtn = document.querySelectorAll('.btn');
+const hasibCalculator = document.querySelectorAll('.calculator')[0];
 
 function calcWrite(btnvalue){
     disp.textContent += btnvalue;
@@ -13,4 +17,16 @@ function doit(){
     var result = eval(disp.textContent);
     disp.textContent = result;
     console.log("value is " + result)
+}
+
+function modeChange(){
+    modeIcon.classList.toggle("active");
+    hasibBody.classList.toggle("nightMood");
+    hasibCalculator.classList.toggle("nightMood");
+
+    for ( i = 0; i <= hasibBtn.length; i++) {
+        hasibBtn[i].classList.toggle("nightMood")
+        
+    }
+    hasibBtn.classList.toggle("nightMood")
 }
